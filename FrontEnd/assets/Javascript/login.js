1a70e4618174abe86a31665708eee7b8fb7032a6
Ines Dumeige <ines.dumeige@gmail.com>
2023-05-04 16:06:45 +0200

    Probleme

--- FrontEnd/assets/Javascript/login.js
+++ FrontEnd/assets/Javascript/login.js
@@ -1,33 +1,44 @@
 const emailCorrect = "sophie.bluel@test.tld";
 const pswCorrect = "S0phie";
 
-// Récupérons les champs d'identifiant et de mot de passe du formulaire HTML
-const emailInput = document.getElementById("email");
-const pswInput = document.getElementById("password");
-
-// Récupérons le bouton de soumission du formulaire HTML
+// Récupérons le formulaire HTML
 const formulaire = document.getElementById("login");
-const boutonSoumettre = document.getElementById("connect");
 
 // Définissons une fonction qui sera appelée lors de la soumission du formulaire
 function soumettreFormulaire(event) {
     event.preventDefault(); // Empêche le formulaire de se soumettre
 
-    // Récupérons les valeurs entrées par l'utilisateur dans les champs d'email et de mot de passe
-    const email = emailInput.value;
-    const psw = pswInput.value;
+    // Créons un objet FormData à partir du formulaire
+    const formData = new FormData(formulaire);
 
-    // Vérifions si l'email et le mot de passe sont corrects
-    if (email === emailCorrect && psw === pswCorrect) {
-        // Si c'est le cas, redirigeons l'utilisateur vers la page avec les boutons d'actions
-        window.location.href = "index.html";
-    } else {
-        // Sinon, affichons un message d'erreur
-        const messageErreur = document.getElementById("wrong-psw");
-        messageErreur.textContent = "Erreur dans l'email ou le mot de passe";
-    }
+    // Récupérons les valeurs entrées par l'utilisateur dans les champs d'email et de mot de passe
+    const email = formData.get("email");
+    const psw = formData.get("password");
+
+    // Récupérons le token d'authentification depuis le localStorage
+    const token = localStorage.getItem("token");
+
+    // Créons une requête AJAX pour vérifier les informations de connexion
+    fetch(`http://localhost:5678/api/users/login`, {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Erreur dans l'identifiant ou le mot de passe");
+            }
+        })
+        .then(data => {
+            localStorage.setItem("token", data.token);
+            window.location.href = "index.html";
+        })
+        .catch(error => {
+            const messageErreur = document.getElementById("wrong-psw");
+            messageErreur.textContent = error.message;
+        });
 }
 
-// Ajoutons un écouteur d'événements sur le bouton de soumission
-boutonSoumettre.addEventListener("click", soumettreFormulaire);
-
+// Ajoutons un écouteur d'événements sur le formulaire
+formulaire.addEventListener("submit", soumettreFormulaire);
