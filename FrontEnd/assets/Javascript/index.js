ec6c4d336668a62911745dd187fc765a4150ff65
Ines Dumeige <ines.dumeige@gmail.com>
2023-05-10 18:40:34 +0200

    Modal + login

--- FrontEnd/assets/Javascript/index.js
+++ FrontEnd/assets/Javascript/index.js
@@ -28,7 +28,6 @@ const fetchWorks = async () => {
     }
 };
 
-
 const fetchWorksEdit = async () => {
     const response = await fetch('http://localhost:5678/api/works');
     const data = await response.json();
@@ -52,7 +51,10 @@ const fetchWorksEdit = async () => {
 
             const iconTrashElement = document.createElement('i');
             iconTrashElement.classList.add('fa-solid', 'fa-trash-can', 'absolute', 'delete');
+            iconTrashElement.id = work.id;
             workElement.appendChild(iconTrashElement);
+            iconTrashElement.addEventListener('click', () => deleteWork(work.id)
+            );
 
             const titleElement = document.createElement('p');
             titleElement.textContent = 'éditer';
@@ -129,31 +131,31 @@ const fetchCategories = async () => {
 
 
 
-const deleteWork = async () => {
-    const deleteIcons = document.querySelectorAll('.fa-trash-can');
-    deleteIcons.forEach(icon => {
-        icon.addEventListener('click', async (event) => {
-            const workElement = event.target.closest('.imgEdit');
-            const imageElement = workElement.querySelector('img');
-            const imageSrc = imageElement.getAttribute('src');
-            const workId = workElement.dataset.workId;
+const deleteWork = async (id) => {
+    //const deleteIcons = document.querySelectorAll('.fa-trash-can');
+    console.log(id);
+    // deleteIcons.forEach(icon => {
+    //         const workElement = event.target.closest('.imgEdit');
+    //         console.log(icon.id);
+    //         console.log('ok');
+    //         const imageElement = workElement.querySelector('img');
+    //         const imageSrc = imageElement.getAttribute('src');
+    //         const workId = workElement.dataset.workId;
 
-            const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
-                method: 'DELETE',
-                headers: {
-                    accept: "application/json",
-                    'Content-Type': "application/json",
-                    'Authorization': `Bearer ${tokenValue}`
-                }
-            });
-            if (response.ok) {
-                workElement.remove();
-                console.log('Supprimé');
-            } else {
-                console.error(`HTTP error! Status: ${response.status}`);
-            }
-        });
+    const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+        method: 'DELETE',
+        headers: {
+            //accept: "application/json",
+            'Content-Type': "application/json",
+            'Authorization': `Bearer ${tokenValue}`
+        }
     });
+    if (response.ok) {
+        //workElement.remove();
+        console.log('Supprimé');
+    } else {
+        console.error(`HTTP error! Status: ${response.status}`);
+    }
 };
 
 function adminMode() {
@@ -170,7 +172,6 @@ function adminMode() {
     console.log(filterContainer);
 };
 
-
 const loginElement = document.getElementById('logout');
 
 if (localStorage.getItem('token')) {
@@ -185,33 +186,125 @@ loginElement.addEventListener('click', () => {
         loginElement.textContent = 'Login';
     }
 });
-//ouvrir modal
-change.addEventListener('click', () => {
-    const myModal = document.getElementById('modalEdit');
-    myModal.style.display = "block";
 
-    const close = document.querySelector('.close');
-    close.addEventListener('click', () => {
-        myModal.style.display = "none";
 
-    });
-    const buttonEdit = document.getElementById('buttonEdit');
-    const modalFile = document.getElementById('modalFile');
-    buttonEdit.addEventListener('click', () => {
-        myModal.style.display = "none";
-        modalFile.style.display = "block";
-    });
+
+
+
+const modalEdit = document.getElementById('modalEdit');
+const openerEdit = document.getElementById('change');
+let closeEdit = document.querySelector('#modalEdit .close');
+const buttonEdit = document.getElementById('buttonEdit');
+const modalFile = document.getElementById('modalFile');
+let closeFile = document.querySelector('#modalFile .close');
+let currentModal = null; // stocke la référence au modal actuellement ouvert
+
+// Ajouter un écouteur d'événement au document
+document.addEventListener('click', function (e) {
+    // Vérifier si l'élément cliqué est le bouton d'ouverture du modal "modalEdit"
+    if (e.target === openerEdit) {
+        if (currentModal !== modalEdit) { // si un autre modal est ouvert
+            if (currentModal) currentModal.style.display = 'none'; // fermer le modal actuel
+            modalEdit.style.display = 'block'; // ouvrir le modal "modalEdit"
+            currentModal = modalEdit; // mettre à jour la référence au modal actuel
+        }
+    }
+    // Vérifier si l'élément cliqué est le bouton d'ouverture du modal "modalFile"
+    else if (e.target === buttonEdit) {
+        if (currentModal !== modalFile) { // si un autre modal est ouvert
+            if (currentModal) currentModal.style.display = 'none'; // fermer le modal actuel
+            modalFile.style.display = 'block'; // ouvrir le modal "modalFile"
+            currentModal = modalFile; // mettre à jour la référence au modal actuel
+        }
+    }
+    // Fermer le modal actuel si on clique en dehors de celui-ci
+    else if (currentModal && ((!currentModal.contains(e.target) && e.target !== closeEdit) || (currentModal === modalFile && e.target === closeFile))) {
+        currentModal.style.display = 'none';
+        currentModal = null; // mettre à jour la référence au modal actuel
+    }
 });
 
 
 
 
-// appel des fonctions
-fetchWorks();
-fetchWorksEdit();
-fetchCategories();
-adminMode();
-deleteWork();
+//test
+function fetchAddWork() {
+    const titreInput = document.getElementById('titre');
+    const categorieInput = document.getElementById('choix');
+    const sendInput = document.getElementById('send');
+    const addFile = document.getElementById('file');
 
-// exemple d'utilisation de la fonction deleteWork lorsqu'un bouton est cliqué
+    const formData = new FormData();
+    formData.append('title', titreInput.value);
+    formData.append('category', categorieInput.value);
+    formData.append('image', addFile.files[0]);
+    const previewDiv = document.getElementById('preview');
+
+    titreInput.addEventListener('input', (e) => {
+        titreInput = e.target.value;
+    });
+
+    categorieInput.addEventListener('input', (e) => {
+        categorieInput = e.target.selectedIndex;
+    });
+
+    if (previewDiv && titreInput && categorieInput) {
+        fetch('http://localhost:5678/api/works', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${tokenValue}`,
+            },
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Ajout de travail réussi :', data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    } else {
+        console.error('Tous les champs doivent être remplis');
+    }
+};
+
+
+
+
+
+//voir l'image
+const fileInput = document.getElementById("file");
+const previewDiv = document.getElementById("preview");
+
+fileInput.addEventListener("change", function () {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+            previewDiv.innerHTML = `<img src="${this.result}" alt="preview image">`;
+        });
+
+        reader.readAsDataURL(file);
+
+        // Hide the upload icon
+        const uploadIcon = document.querySelector("#fileImg i");
+        uploadIcon.style.display = "none";
+    }
+});
+
+
+// appel des fonctions
+
+function initScript() {
+    fetchWorks();
+    fetchWorksEdit();
+    fetchCategories();
+    adminMode();
+    deleteWork();
+    fetchAddWork();
+}
+initScript();
+
 
